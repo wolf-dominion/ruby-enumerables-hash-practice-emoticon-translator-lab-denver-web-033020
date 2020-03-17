5afa673c9db15719cf5a2bcb5e05ba40f3a889e9
Hannah Drury <hannah_drury@yahoo.com>
2020-03-17 19:37:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ def load_library(emoticonsFile)
           emoticonsSorted['get_meaning'][emoHash[1]] = english
        
         
-          emoticonsSorted['get_emoticon'][n] = n+1
+          emoticonsSorted['get_emoticon'][emoHash[0]] = emoHash[1]
     end
   pp emoticonsSorted
   emoticonsSorted
