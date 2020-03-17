8d1187a635da0098d9bcdf5f6dc052478af9256e
Hannah Drury <hannah_drury@yahoo.com>
2020-03-17 19:19:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,6 +7,12 @@ def load_library(emoticonsFile)
   library = YAML.load_file(emoticonsFile)
   
   pp library
+  
+  emoticonsSorted = {'get_meaning' => {}, 'get_emoticon' => {}}
+  
+  library.each do |english, emoHash|
+    
+  end
 
 end
 
