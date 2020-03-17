f84a1f37fc0541863b0ac9d4609d1e5097b23aeb
Hannah Drury <hannah_drury@yahoo.com>
2020-03-17 19:58:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(emoticonsFile, eEmoticon)
   sortedFile['get_emoticon'][eEmoticon]
   
   if !sortedFile['get_emoticon'][eEmoticon]
-    return "Sorry, none"
+    return "Sorry, that emoticon was not found"
   end
 
 end
